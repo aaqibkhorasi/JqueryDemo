82d3f56c0f2b152167f56f494fd37a9a449789fd
Aaqib Khorasi <akhorasi@sfu.ca>
2015-06-21 19:46:46 -0700

    modified some files and error

--- Events/events.js
+++ Events/events.js
@@ -1,35 +1,7 @@
 $(function() {
-//            binding on and binding off 
-//            takes event and function
-        // MOUSE EVENT 
-
-    $("#boxTarget").on("mouseover mouseleave", boxcolor);
-    $("#boxTarget").on("click", function(evt) {
-        $("#boxTarget").off("mouseover mouseleave", boxcolor);
-        $("#boxTarget").html("<p>You shut off the hover effect!</p>");
-        $("#boxTarget").removeClass("boxColor");
-    });   
     
-    // offset property of any DOM ELEMENT 
-    $(document).on('click','.search', function(event) {
-        event.preventDefault();
-        var target = "#" + this.getAttribute('data-target');
-        $('html, body').animate({
-            scrollTop: $(target).offset().top
-        }, 2000);
-    });
     
-    //HOVER 
-    $("div.subContent").hover(highlight) ;  
-    $("div.subContent").click(function(){
-        $(this).css("border", "2px solid red");
-    }); 
-    
-    // KEY PRESS EVENT 
-    $("#textEntry").keypress(function(evt){
-       $("#keyPress").text(String.fromCharCode(evt.charCode)); 
-    });
-    
+      
     //SLIDE DOWN AND SLIDETOGGLE
     $("#slideDown").click(function(){
         if ($("#slider").is(":hidden")){
@@ -43,20 +15,52 @@ $(function() {
     $("#effect").click(function(){
         $("#slider").fadeOut('slow') ; 
     });
+    // --------------------------------------------------------------------------------
+//            binding on and binding off 
+//            takes event and function
+        // MOUSE EVENT 
+
+    $("#boxTarget").on("mouseover mouseleave", boxcolor);
+    $("#boxTarget").on("click", function(evt) {
+        $("#boxTarget").off("mouseover mouseleave", boxcolor);
+        $("#boxTarget").html("<p>You shut off the hover effect!</p>");
+        $("#boxTarget").removeClass("boxColor");
+    });   
     
+    //------------------------------------------------------------------------------
+    // offset property of any DOM ELEMENT 
+    $(document).on('click','.search', function(event) {
+        event.preventDefault();
+        var target = "#" + this.getAttribute('data-target');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 2000);
+    });
+    
+    $("#up").click(goup);
+    
+    //HOVER 
+    $("div.subContent").hover(highlight) ;  
+    $("div.subContent").click(function(){
+        $(this).css("border", "2px solid red");
+    }); 
+    
+    // KEY PRESS EVENT 
+    $("#textEntry").keypress(function(evt){
+       $("#keyPress").text(String.fromCharCode(evt.charCode)); 
+    });
+  
 
     
-    
-    
-   /* // key event
-    $("#textEntry").on("keypress", function(evt) {
-        $("#keyPress").text(String.fromCharCode(evt.charCode));
-    });*/
+
 });
 //        function slider() { 
 //            
 //            }
 //        }
+function goup(){
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+}
 function boxcolor(){
     $(this).toggleClass("boxColor");
 }
